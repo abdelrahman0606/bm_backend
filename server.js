3e079f5f3530d221c265809618bec4f6d6904266
Abdo73873 <abdelrahman73873.com>
2025-10-12 23:20:10 +0400

    ip address

--- server.js
+++ server.js
@@ -1,11 +1,47 @@
-const express = require('express');
+// Initiate Express Server
+const express = require("express");
 const app = express();
-const port = 3000;
+const path = require("path");
 
-app.get("/", (req, res) => {
-  res.send("Hello World www");
+const server = require("http").createServer(app);
+
+
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (req.method === "OPTIONS") {
+    // if (allowedOrigins.includes(origin)) {
+      res.setHeader("Access-Control-Allow-Origin", origin);
+      res.setHeader("Access-Control-Allow-Credentials", "true");
+    // }
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Max-Age", "86400");
+    return res.sendStatus(200);
+  }
+  next();
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+
+app.use("/ReportServer", (req, res) => {
+  res.status(403).json({ error: "Access denied" });
 });
+
+
+app.get("/test", (req, res, next) => {
+  res.send("App express on vrecel is done");
+});
+
+
+app.use("/attachments", express.static(path.join(__dirname, "./attachments")));
+app.use(express.static("public"));
+
+const port = process.env.PORT || 3003;
+const host = "0.0.0.0";
+
+server.listen(port, host, function () {
+  console.info(`Server is listening on http://${host}:${port}`);
+});
+
+module.exports = server;
+
